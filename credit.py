ed0aa2b54fcaad62a3658317d266038127e5aac0
rahulomprasad <61745278+rahulomprasad@users.noreply.github.com>
2020-10-10 07:58:23 +0530

    Add files via upload

---
+++ credit.py
@@ -0,0 +1,255 @@
+import pandas as pd 
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%matplotlib inline 
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+from plotly import tools
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+from datetime import datetime 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from catboost import CatBoostClassifier
+from sklearn import svm
+#import lightgbm as lgb
+#from lightgbm import LGBMClassifier
+import xgboost as xgb
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+
+
+
+
+RFC_METRIC = 'gini'  #metric used for RandomForrestClassifier
+NUM_ESTIMATORS = 100 #number of estimators used for RandomForrestClassifier
+NO_JOBS = 4 #number of parallel jobs used for RandomForrestClassifier
+
+
+#TRAIN/VALIDATION/TEST SPLIT
+#VALIDATION
+VALID_SIZE = 0.20 # simple validation using train_test_split
+TEST_SIZE = 0.20 # test size using_train_test_split
+
+#CROSS-VALIDATION
+NUMBER_KFOLDS = 5 #number of KFolds for cross-validation
+
+
+
+RANDOM_STATE = 2018
+
+
+MAX_ROUNDS = 1000 #lgb iterations
+EARLY_STOP = 50 #lgb early stop 
+OPT_ROUNDS = 1000  #To be adjusted based on best validation rounds
+VERBOSE_EVAL = 50 #Print out metric result
+
+#data_df = pd.read_csv(PATH+"/creditcard.csv")
+data_df = pd.read_csv("creditcard.csv")
+
+#::::CHECK MISSING DATA::::
+
+total = data_df.isnull().sum().sort_values(ascending = False)
+percent = (data_df.isnull().sum()/data_df.isnull().count()*100).sort_values(ascending = False)
+pd.concat([total, percent], axis=1, keys=['Total', 'Percent']).transpose()
+
+#::::data unbalance::::
+
+temp = data_df["Class"].value_counts()
+df = pd.DataFrame({'Class': temp.index,'values': temp.values})
+
+trace = go.Bar(
+    x = df['Class'],y = df['values'],
+    name="Credit Card Fraud Class - data unbalance (Not fraud = 0, Fraud = 1)",
+    marker=dict(color="Red"),
+    text=df['values']
+)
+data = [trace]
+layout = dict(title = 'Credit Card Fraud Class - data unbalance (Not fraud = 0, Fraud = 1)',
+          xaxis = dict(title = 'Class', showticklabels=True), 
+          yaxis = dict(title = 'Number of transactions'),
+          hovermode = 'closest',width=600
+         )
+fig = dict(data=data, layout=layout)
+iplot(fig, filename='class')
+
+#transection amount::::
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12,6))
+s = sns.boxplot(ax = ax1, x="Class", y="Amount", hue="Class",data=data_df, palette="PRGn",showfliers=True)
+s = sns.boxplot(ax = ax2, x="Class", y="Amount", hue="Class",data=data_df, palette="PRGn",showfliers=False)
+plt.show();
+tmp = data_df[['Amount','Class']].copy()
+class_0 = tmp.loc[tmp['Class'] == 0]['Amount']
+class_1 = tmp.loc[tmp['Class'] == 1]['Amount']
+class_0.describe()
+class_1.describe()
+
+#train test split::::
+
+train_df, test_df = train_test_split(data_df, test_size=TEST_SIZE, random_state=RANDOM_STATE, shuffle=True )
+train_df, valid_df = train_test_split(train_df, test_size=VALID_SIZE, random_state=RANDOM_STATE, shuffle=True )
+
+#naive bayes classification::
+
+# We define the model
+nbcla = GaussianNB()
+X_train=train_df.drop(['Class'],axis=1)
+Y_train=train_df['Class']
+X_test=test_df.drop(['Class'],axis=1)
+Y_test=test_df['Class']
+# We train model
+#clf.fit(train_df[predictors], train_df[target].values,verbose=True)
+nbcla.fit(X_train, Y_train)
+
+# We predict target values
+#pred = nbcla.predict(X_test)
+#preds = clf.predict(valid_df[predictors])
+
+# # We predict target values
+Y_predict3 = nbcla.predict(X_test)
+
+test_acc_nbcla  = round(nbcla .fit(X_train,Y_train).score(X_test, Y_test)* 100, 2)
+train_acc_nbcla  = round(nbcla .fit(X_train, Y_train).score(X_train, Y_train)* 100, 2)
+
+# The confusion matrix
+nbcla_cm = confusion_matrix(Y_test, Y_predict3)
+f, ax = plt.subplots(figsize=(5,5))
+sns.heatmap(nbcla_cm, annot=True, linewidth=0.7, linecolor='black', fmt='g', ax=ax, cmap="BuPu")
+plt.title('Naive Bayes Classification Confusion Matrix')
+plt.xlabel('Y predict')
+plt.ylabel('Y test')
+plt.show()
+
+model1 = pd.DataFrame({
+    'Model': ['Naive Bayes'],
+    'Train Score': [train_acc_nbcla],
+    'Test Score': [test_acc_nbcla]
+})
+model1.sort_values(by='Test Score', ascending=False)
+
+
+target = 'Class'
+predictors = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',\
+       'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19',\
+       'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28',\
+       'Amount']
+
+
+
+#random forest classifier::
+
+clf = RandomForestClassifier(n_jobs=NO_JOBS, 
+                             random_state=RANDOM_STATE,
+                             criterion=RFC_METRIC,
+                             n_estimators=NUM_ESTIMATORS,
+                             verbose=False)
+
+clf.fit(train_df[predictors], train_df[target].values)
+preds = clf.predict(valid_df[predictors])
+
+#feature importance::
+
+tmp = pd.DataFrame({'Feature': predictors, 'Feature importance': clf.feature_importances_})
+tmp = tmp.sort_values(by='Feature importance',ascending=False)
+plt.figure(figsize = (7,4))
+plt.title('Features importance',fontsize=14)
+s = sns.barplot(x='Feature',y='Feature importance',data=tmp)
+s.set_xticklabels(s.get_xticklabels(),rotation=90)
+plt.show()   
+
+#confusion matrices::
+
+cm = pd.crosstab(valid_df[target].values, preds, rownames=['Actual'], colnames=['Predicted'])
+fig, (ax1) = plt.subplots(ncols=1, figsize=(5,5))
+sns.heatmap(cm, 
+            xticklabels=['Not Fraud', 'Fraud'],
+            yticklabels=['Not Fraud', 'Fraud'],
+            annot=True,ax=ax1,
+            linewidths=.2,linecolor="Darkblue", cmap="Blues")
+plt.title('Confusion Matrix', fontsize=14)
+plt.show()
+
+#accuracy::
+roc_auc_score(valid_df[target].values, preds)
+
+#using catbooster :::
+
+clf = CatBoostClassifier(iterations=500,
+                             learning_rate=0.02,
+                             depth=12,
+                             eval_metric='AUC',
+                             random_seed = RANDOM_STATE,
+                             bagging_temperature = 0.2,
+                             od_type='Iter',
+                             metric_period = VERBOSE_EVAL,
+                             od_wait=100)
+
+clf.fit(train_df[predictors], train_df[target].values,verbose=50)
+preds = clf.predict(valid_df[predictors])
+
+tmp = pd.DataFrame({'Feature': predictors, 'Feature importance': clf.feature_importances_})
+tmp = tmp.sort_values(by='Feature importance',ascending=False)
+plt.figure(figsize = (7,4))
+plt.title('Features importance',fontsize=14)
+s = sns.barplot(x='Feature',y='Feature importance',data=tmp)
+s.set_xticklabels(s.get_xticklabels(),rotation=90)
+plt.show()   
+
+cm = pd.crosstab(valid_df[target].values, preds, rownames=['Actual'], colnames=['Predicted'])
+fig, (ax1) = plt.subplots(ncols=1, figsize=(5,5))
+sns.heatmap(cm, 
+            xticklabels=['Not Fraud', 'Fraud'],
+            yticklabels=['Not Fraud', 'Fraud'],
+            annot=True,ax=ax1,
+            linewidths=.2,linecolor="Darkblue", cmap="Blues")
+plt.title('Confusion Matrix', fontsize=14)
+plt.show()
+
+roc_auc_score(valid_df[target].values, preds)
+
+#usinf XGboost:::
+
+# Prepare the train and valid datasets
+dtrain = xgb.DMatrix(train_df[predictors], train_df[target].values)
+dvalid = xgb.DMatrix(valid_df[predictors], valid_df[target].values)
+dtest = xgb.DMatrix(test_df[predictors], test_df[target].values)
+
+#What to monitor (in this case, **train** and **valid**)
+watchlist = [(dtrain, 'train'), (dvalid, 'valid')]
+
+# Set xgboost parameters
+params = {}
+params['objective'] = 'binary:logistic'
+params['eta'] = 0.039
+params['silent'] = True
+params['max_depth'] = 2
+params['subsample'] = 0.8
+params['colsample_bytree'] = 0.9
+params['eval_metric'] = 'auc'
+params['random_state'] = RANDOM_STATE
+
+model = xgb.train(params, 
+                dtrain, 
+                MAX_ROUNDS, 
+                watchlist, 
+                early_stopping_rounds=EARLY_STOP, 
+                maximize=True, 
+                verbose_eval=VERBOSE_EVAL)
+
+#plot variable importance::
+
+fig, (ax) = plt.subplots(ncols=1, figsize=(8,5))
+xgb.plot_importance(model, height=0.8, title="Features importance (XGBoost)", ax=ax, color="green") 
+plt.show()
+
+preds = model.predict(dtest)
+
+roc_auc_score(test_df[target].values, preds)
+
